997290f87fb1f4948146546c539cda7e62fc8c78
LautaroChoque <104146948+LautaroChoque@users.noreply.github.com>
2022-07-19 19:57:27 -0300

    terminando

---
+++ main.js
@@ -0,0 +1,32 @@
+
+class Criptomonedas{
+    constructor (id, titulo, imagen, precio){
+        this.id = id
+        this.titulo = titulo
+        this.imagen = imagen
+        this.precio = precio
+    }
+}
+
+const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000)
+const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000)
+const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0,5)
+const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000)
+
+const criptomonedas = [bitcoin, ethereum, ada, bnb]
+
+const divCriptomonedas = document.getElementById("divCriptomonedas")
+
+criptomonedas.forEach(bitcoin => {
+    divCriptomonedas.innerHTML += `
+    <div id= "criptomonedas ${bitcoin.id}" class="criptos" >
+        <p class="titulo"> ${bitcoin.titulo} </p>
+        <img class="imagenes" src= ${bitcoin.imagen} </img>
+        <p class="texto" >Precio: ${bitcoin.precio} </p>
+    </div>
+    `
+})
+
+
+
+
