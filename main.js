bb1389d72ce55a59ff03c3b509041f324c3fa975
LautaroChoque <104146948+LautaroChoque@users.noreply.github.com>
2022-08-09 19:49:08 -0300

    prueba

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 
 class Criptomonedas{
-    constructor (id, titulo, imagen, precio){
+    constructor (id, titulo, imagen, precio, boton){
         this.id = id
         this.titulo = titulo
         this.imagen = imagen
         this.precio = precio
+        this.boton = boton
     }
 }
 
-const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000)
-const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000)
-const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0,5)
-const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000)
+const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000, "Comprar")
+const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000, "Comprar")
+const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0.5, "Comprar")
+const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000, "Comprar")
 
 const criptomonedas = [bitcoin, ethereum, ada, bnb]
 
@@ -26,21 +27,42 @@ criptomonedas.forEach(bitcoin => {
         <p class="titulo"> ${bitcoin.titulo} </p>
         <img class="imagenes" src= ${bitcoin.imagen} </img>
         <p class="texto" >Precio: ${bitcoin.precio} </p>
-        <button class="" id="">Comprar</button>
+        <button class="botComprar" id=""> ${bitcoin.boton}</button>
     </div>
     `
 })
 
 const botonComprar = document.getElementById("botonComprar")
-
     botonComprar.addEventListener("click", () => {
-        console.log("Click en comprar")
+        Swal.fire({
+            icon: 'success',
+            title: 'Compraste',
+            text: 'Tu compra ha sido finalizada!',
+            footer: '<a Mas informacion</a>'
+          })
     })
 
 const botonVender = document.getElementById("botonVender")
     botonVender.addEventListener("click", () =>{
-        console.log("Click en vender")
+        Swal.fire({
+            icon: 'success',
+            title: 'Vendiste',
+            text: 'Tu venta ha sido finalizada',
+            footer: '<a Mas informacion</a>'
+          })
     })
+
+const boton = document.getElementsByClassName("botComprar")
+    boton.addEventListener("click", () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Compraste',
+            text: 'Tu compra ha sido finalizada!',
+            footer: '<a Mas informacion</a>'
+          })
+    console.log("botComprar")
+        })
+    
 
 const input = document.getElementById("input")
 
@@ -58,4 +80,5 @@ if(localStorage.getItem('theme')){
 
 } else{
     localStorage.setItem('theme', 'light')
-} 
+}  
+
