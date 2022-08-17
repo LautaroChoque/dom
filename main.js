09e7f50f2004ebe73fe1450f04591fc114677da9
LautaroChoque <104146948+LautaroChoque@users.noreply.github.com>
2022-08-17 15:35:38 -0300

    agregando api

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 
-class Criptomonedas{
-    constructor (id, titulo, imagen, precio, boton){
-        this.id = id
-        this.titulo = titulo
-        this.imagen = imagen
-        this.precio = precio
-        this.boton = boton
-    }
-}
+// class Criptomonedas{
+//     constructor (id, titulo, imagen, precio, boton){
+//         this.id = id
+//         this.titulo = titulo
+//         this.imagen = imagen
+//         this.precio = precio
+//         this.boton = boton
+//     }
+// }
 
-const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000, "Comprar")
-const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000, "Comprar")
-const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0.5, "Comprar")
-const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000, "Comprar")
+// const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000, "Comprar")
+// const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000, "Comprar")
+// const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0.5, "Comprar")
+// const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000, "Comprar")
 
-const criptomonedas = [bitcoin, ethereum, ada, bnb]
+// const criptomonedas = [bitcoin, ethereum, ada, bnb]
 
-localStorage.setItem('criptomonedas', JSON.stringify(criptomonedas))
-console.log(JSON.parse(localStorage.getItem('criptomonedas')))
+// localStorage.setItem('criptomonedas', JSON.stringify(criptomonedas))
+// console.log(JSON.parse(localStorage.getItem('criptomonedas')))
 
-const divCriptomonedas = document.getElementById("divCriptomonedas")
+// const divCriptomonedas = document.getElementById("divCriptomonedas")
     
-    fetch('https://criptoya.com/api/ripio/btc')
-.then(response => response.json())
-.then(({ask}) => {
-    criptomonedas.forEach(bitcoin => {
-        divCriptomonedas.innerHTML += `
-        <div id= "criptomonedas ${bitcoin.id}" class="criptos" >
-            <p class="titulo"> ${bitcoin.titulo} </p>
-            <img class="imagenes" src= ${bitcoin.imagen} </img>
-            <p class="texto" >Precio: ${ask} </p>
-            <button class="botComprar" id=""> ${bitcoin.boton}</button>
-        </div>
-        `
-    })   
-})
+//     fetch('https://criptoya.com/api/ripio/btc')
+// .then(response => response.json())
+// .then(({ask}) => {
+//     criptomonedas.forEach(bitcoin => {
+//         divCriptomonedas.innerHTML += `
+//         <div id= "criptomonedas ${bitcoin.id}" class="criptos" >
+//             <p class="titulo"> ${bitcoin.titulo} </p>
+//             <img class="imagenes" src= ${bitcoin.imagen} </img>
+//             <p class="texto" >Precio: ${ask} </p>
+//             <button class="botComprar" id=""> ${bitcoin.boton}</button>
+//         </div>
+//         `
+//     })   
+// })
 
 
 
@@ -64,18 +64,52 @@ const divCriptomonedas = document.getElementById("divCriptomonedas")
 // })
 
 
-const boton = document.getElementsByClassName("botComprar")
-const botones = [...boton];
-    botones.forEach((elem)=>{
-        elem.addEventListener('click', () => {
-            Swal.fire({
-                icon: 'success',
-                title: 'Compraste',
-                text: 'Tu compra ha sido finalizada',
-                footer: '<a Mas informacion</a>'
-              })
-        })
-    }) 
+class Criptomonedas{
+    constructor (id, titulo, imagen, precio, boton){
+        this.id = id
+        this.titulo = titulo
+        this.imagen = imagen
+        this.precio = precio
+        this.boton = boton
+    }
+}
+
+const bitcoin = new Criptomonedas(1, "Bitcoin", "img/opengraph.png", 79000, "Comprar")
+const ethereum = new Criptomonedas(2, "Ethereum", "img/eth.png", 9000, "Comprar")
+const ada = new Criptomonedas(3, "Cardano", "img/ada.png", 0.5, "Comprar")
+const bnb = new Criptomonedas(4, "BNB", "img/bnb.png", 12000, "Comprar")
+
+const criptomonedas = [bitcoin, ethereum, ada, bnb]
+
+localStorage.setItem('criptomonedas', JSON.stringify(criptomonedas))
+console.log(JSON.parse(localStorage.getItem('criptomonedas')))
+
+const divCriptomonedas = document.getElementById("divCriptomonedas")
+
+async function infoapi(linkApi,indice){
+    const infoapi = await fetch(linkApi)
+    const criptoParseado = await infoapi.json()
+    criptomonedas[indice].precio = criptoParseado.ask
+    console.log(criptomonedas)
+    divCriptomonedas.innerHTML += `
+    <div id= "criptomonedas ${criptomonedas[indice].id}" class="criptos" >
+        <p class="titulo"> ${criptomonedas[indice].titulo} </p>
+        <img class="imagenes" src= ${criptomonedas[indice].imagen} </img>
+        <p class="texto" >Precio: ${criptomonedas[indice].precio} </p>
+        <button class="botonComprar1">${criptomonedas[indice].boton}</button>
+    </div>
+    `
+}
+
+infoapi('https://criptoya.com/api/ripio/btc', 0).then
+infoapi('https://criptoya.com/api/ripio/eth', 1).then
+infoapi('https://criptoya.com/api/ripio/ada', 2).then
+
+
+
+
+
+     
 
 const botonComprar = document.getElementById("botonComprar")
     botonComprar.addEventListener("click", () => {
@@ -100,7 +134,7 @@ const botonVender = document.getElementById("botonVender")
 
     
     
-console.log(botones)
+
 const input = document.getElementById("input")
 
 
